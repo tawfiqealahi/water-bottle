6a8f803c32b8b08bd1a0cb239774a053f5133ea9
tawfiq <tawfiqealahi99@gmail.com>
2024-10-30 23:25:02 +0600

    grid bottle

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -4,7 +4,7 @@ const Bottle = ({bottle}) => {
     const{name,img,price}=bottle;
     return (
         <div className='bottle'>
-            <h3>Bottle name: {name}</h3>
+            <h4>Bottle name: {name}</h4>
             <img src={img} alt="" />
             <p>${price}</p>
         </div>
